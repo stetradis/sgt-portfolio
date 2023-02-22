d19aeaff358efb74928c9629bf60dce4e04797e8
Stella Tetradis <stella.tetradis@gmail.com>
2023-02-22 11:18:00 -0500

    tailwind config and cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Space Grotesk', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
