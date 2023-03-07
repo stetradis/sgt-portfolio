cd9d498a0325c64a85967437ca8622ad5db3323a
Stella Tetradis <stella.tetradis@gmail.com>
2023-03-07 11:13:10 -0500

    update stack styling

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -9,44 +9,43 @@ const TechStack = () => {
                 <p className='text-md py-4 text-gray-600 dark:text-gray-300 max-w-xl mx-auto'>The tech stack that I use consists of a combination of programming languages, frameworks, libraries, and tools that enable me to build robust and scalable applications. This is an overview of my experience with various technologies that I have used throughout my career. </p>
             </div>
             <div className='flex flex-wrap gap-4'>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiCsharp className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiDotnet className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiJavascript className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiTypescript className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiReact className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiNextdotjs className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <AiOutlineConsoleSql className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiGithub className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiAngularjs className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiVuedotjs className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiAmazonaws className='icon' size={40}/>
                 </div>
-                <div className="stack block max-w-xs p-6 darkcard mx-auto rounded overflow-hidden border relative shadow-md">
+                <div className="stack">
                     <SiAzuredevops className='icon' size={40}/>
                 </div>
             </div>
-            
         </section>
     );
 }
